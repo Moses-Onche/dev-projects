11bd47a1999ee6842e07043ab7c7516b7beb6066
Moses-Onche <onchemoses38@gmail.com>
2024-12-27 11:48:41 +0100

    Convert Dev Projects into a Git repository

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import ImgGen from "./projects/01-img-gen/main";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ul>
+        <li>
+          <a href={ImgGen}>Alpaca Image Generator</a>
+        </li>
+      </ul>
+      <ImgGen />
+    </>
+  )
+}
+
+export default App
---
+++ src/projects/01-img-gen/main.jsx
@@ -0,0 +1,10 @@
+const alpacaPage = () => {
+    return (
+        <>
+            <h1>Alpaca Generator</h1>
+            <div></div>
+        </>
+    )
+}
+
+export default alpacaPage;
