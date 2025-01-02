63afc8250da3fd3b07f657f84a45bb57c16defae
Moses-Onche <onchemoses38@gmail.com>
2025-01-02 12:20:29 +0100

    Build home page and route alpaca image generator page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
-import ImgGen from "./projects/01-img-gen/main";
+import AlpacaImgGen from "./projects/01-img-gen/main";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import alpaca from "./assets/alpaca.png";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Dev Projects</h1>
+      <a href="https://www.github.com/Moses-Onche" className="ml-14 text-slate-900">by @meo_joule</a>
+      <div>
+        <Link to="/alpaca-image-gen">
+          <div>
+            <img src={ alpaca } alt="alpaca image generator" />
+            <h3>Alpaca Image Generator</h3>
+            <span>Reactjs</span>
+            <span>TailwindCSS</span>
+            <span>html2canvas</span>
+          </div>
+        </Link>
+      </div>
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <ul>
-        <li>
-          {/* <a href={ImgGen}>Alpaca Image Generator</a> */}
-        </li>
-      </ul>
-      <ImgGen />
+      {/* <Home /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/alpaca-image-gen" element={ <AlpacaImgGen /> }/>
+        </Routes>
+      </Router>
+      {/* <AlpacaImgGen /> */}
     </>
   )
 }
